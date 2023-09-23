fece935c44062e2b5075c54c1caf9adcd4803e9f
Your Name~ <you@example.com>
2023-09-23 20:34:11 +0530

    in progress

---
+++ Form Validation/script.js
@@ -0,0 +1,6 @@
+const name = document.getElementById("name");
+const phn = document.getElementById("phone-no");
+const email = document.getElementById("email");
+const passwrd = document.getElementById("pass");
+const dob = document.getElementById("dob");
+const loc = document.getElementById("loc");
